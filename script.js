f418239175e931ac2c8576faa82ae7690954f5d9
Beast Code <beastcode9999@gmail.com>
2022-07-27 15:34:32 +0900

    add JS selectors to output and input

---
+++ script.js
@@ -0,0 +1,9 @@
+const outputContainer = document.querySelector('.output-container');
+const output = document.querySelector('[data-output]');
+const inputContainer = document.querySelector('.input-containery');
+
+window.addEventListener('load', createButtons);
+
+function createButtons() {
+    inputContainer
+}
